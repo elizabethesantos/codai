e67ff18168eafddfda03e14bb1fa05adae9c918d
Elizabeth dos Santos <elizabethsantos2402@gmail.com>
2022-03-09 06:09:21 -0300

    d

---
+++ nikel/public/js/home.js
@@ -0,0 +1,60 @@
+const myModal = new bootstrap.Modal("#transaction-modal");// tras a modal para o js
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("sesion");
+let data = {
+    transactions: []
+};
+
+document.getElementById("button-logout").addEventListener("click", logout);
+
+// adicionar lançamento
+document.getElementById("transaction-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const value = parseFloat(document.getElementById("value-input").value);
+    const description = document.getElementById("description-input").value;
+    const date = document.getElementById("date-input").value;
+    const type = document.querySelector('input[name="type-input"]:checked').value;
+
+    data.transactions.unshift({
+        value: value, type: type, description: description, date: date
+    });
+
+    saveData(data);
+    e.target.reset();//limpa o formulario
+    myModal.hide();//fecha a modal
+
+    alert("Lançamento adicionado com sucesso.");
+
+});
+
+checkedLogged();
+
+function checkedLogged() {
+    if(session) {
+        sessionStorage.setItem("logged", session);
+        logged = session;
+    }
+
+    if(!logged) {
+        window.location.href = "index.html";
+        return;
+    }
+
+    const dataUser = localStorage.getItem(logged);
+    if(dataUser) {
+        data = JSON.parse(dataUser);//pegando os dados das transaçoes salvas
+    }
+}
+
+function logout() {
+    sessionStorage.removeItem("logged");
+    localStorage.removeItem("session");
+
+    window.location.href = "index.html"
+}
+
+function saveData(data) {
+    localStorage.setItem(data.login, JSON.stringify(data));
+}
+
